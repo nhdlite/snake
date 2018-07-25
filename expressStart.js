a2e87ce21caeb4e66f08d0dc4743cb10d57352ad
Nate Dessert <ndessert@zulily.com>
2018-07-24 18:50:14 -0700

    Add mobile swipes to app

---
+++ expressStart.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+var path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
